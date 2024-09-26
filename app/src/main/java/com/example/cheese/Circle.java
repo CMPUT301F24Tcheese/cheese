ba29bd31f597adc0fbcfe417f35a0ef364c83163
“Ling <lzhuo2@ualberta.ca>
2024-09-25 19:30:57 -0600

    Added Circle class extending Shape

--- app/src/main/java/com/example/cheese/Circle.java
+++ app/src/main/java/com/example/cheese/Circle.java
@@ -1,15 +1,4 @@
 package com.example.cheese;
 
 public class Circle extends Shape {
-    private int radius;
-
-    public Circle(int x, int y, int radius) {
-        super(x, y);
-        this.radius = radius;
-    }
-
-    @Override
-    public void draw() {
-        System.out.println("Drawing a circle at (" + x + ", " + y + ") with radius " + radius);
-    }
 }
--- app/src/main/java/com/example/cheese/Shape.java
+++ app/src/main/java/com/example/cheese/Shape.java
@@ -3,12 +3,6 @@ package com.example.cheese;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "blue";  // 新添加的字段
 
-    public Shape(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public abstract void draw();
+    String color = "blue"
 }
